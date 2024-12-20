b2c22f12c89ce3e67a848e2e16d43e977969436e
Elena <elen.vasileva@gmail.com>
2024-12-20 02:08:19 +0100

    feat: cats, carbon, team

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== "string") return false;
+  const activity = +sampleActivity;
+  if (Number.isNaN(activity)) return false;
+  if (activity <= 0 || activity > 15) return false;
+  return Math.ceil((Math.log(15 / activity) * 10000) / 1.209425);
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if(!members || !Array.isArray(members)){
+    return '';
+  }
+  const filtered = members.filter((member) => typeof member === "string");
+  return filtered.map((member) => member.trim()[0].toUpperCase()).sort().join("");
 }
 
 module.exports = {
